b01ab1ef6c594741862c907eb9c217754c79ff41
IG <igor.widawski@gmail.com>
2021-01-23 20:07:44 +0100

    Basic Grid implementation

--- PW_Pathfinding/Assets/Scripts/Grid.cs
+++ PW_Pathfinding/Assets/Scripts/Grid.cs
@@ -5,32 +5,82 @@ using CodeMonkey.Utils;
 
 public class Grid
 {
-    int width;
-    int height;
-    int[,] gridArray;
-    float cellSize;
+    int _width;
+    int _height;
+    int[,] _gridArray;
+    float _cellSize;
+    Vector3 _originPosition;
+    TextMesh[,] _debugTextArray;
 
-   public Grid (int width, int height, float cellSize)
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
     {
-        this.width = width;
-        this.height = height;
-        this.cellSize = cellSize;
+        this._width = width;
+        this._height = height;
+        this._cellSize = cellSize;
+        this._originPosition = originPosition;
 
-        gridArray = new int[width, height];
+        _gridArray = new int[width, height];
+        _debugTextArray = new TextMesh[width, height];
 
-        for (int x = 0; x < gridArray.GetLength(0); x++)
+        for (int x = 0; x < _gridArray.GetLength(0); x++)
         {
-            for (int y = 0; y < gridArray.GetLength(1); y++)
+            for (int y = 0; y < _gridArray.GetLength(1); y++)
             {
                 Debug.Log(x + ", " + y);
-                UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y), 20, Color.white, TextAnchor.MiddleCenter);
+                _debugTextArray[x,y] = UtilsClass.CreateWorldText(_gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize, 0) * 0.5f, 20, Color.white, TextAnchor.MiddleCenter);
+                SetValue(x, y, 32);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
             }
         }
+    }
 
-        Vector3 GetWorldPosition(int x, int y)
+    Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * _cellSize + _originPosition;
+    }
+
+    void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - _originPosition).x / _cellSize);
+        y = Mathf.FloorToInt((worldPosition - _originPosition).y / _cellSize);
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < _width && y < _height)
         {
-            return new Vector3(x, y) * cellSize;
+            _gridArray[x, y] = value;
+            _debugTextArray[x, y].text = value.ToString();
         }
     }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < _width && y < _height)
+        {
+            return _gridArray[x, y];
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
 }
+
--- PW_Pathfinding/Assets/Scripts/Testing.cs
+++ PW_Pathfinding/Assets/Scripts/Testing.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Testing : MonoBehaviour
 {
+    Grid _grid;
     // Start is called before the first frame update
     void Start()
     {
-        Grid grid = new Grid(4, 2, 10);
+        _grid = new Grid(4, 2, 10, new Vector3(20,10,0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            _grid.SetValue(worldPosition, 15);
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log("Clicked grid value: " + _grid.GetValue(worldPosition));
+        }
     }
 }
