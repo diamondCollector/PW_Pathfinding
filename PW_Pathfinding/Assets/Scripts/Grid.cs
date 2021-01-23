8cdae5801837c22fe5fed7c25e553998eef4e3c5
IG <igor.widawski@gmail.com>
2021-01-23 18:14:59 +0100

    Initial comit
    - Adding Utils
    - Setting basic grid
    - Setting testing script

---
+++ PW_Pathfinding/Assets/Scripts/Grid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Grid
+{
+    int width;
+    int height;
+    int[,] gridArray;
+    float cellSize;
+
+   public Grid (int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new int[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                Debug.Log(x + ", " + y);
+                UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y), 20, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+            }
+        }
+
+        Vector3 GetWorldPosition(int x, int y)
+        {
+            return new Vector3(x, y) * cellSize;
+        }
+    }
+}
---
+++ PW_Pathfinding/Assets/Scripts/Testing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Grid grid = new Grid(4, 2, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
